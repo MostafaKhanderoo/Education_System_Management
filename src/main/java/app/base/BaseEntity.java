1d62c5c156a7749b0cc97fb9e2f0b752f900e75b
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 16:53:01 +0330

    add add field and Annotation MapedsuperClass

--- src/main/java/app/base/BaseEntity.java
+++ src/main/java/app/base/BaseEntity.java
@@ -1,10 +1,20 @@
 package app.base;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
-
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseEntity<ID extends Serializable> {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private ID id;
 }
