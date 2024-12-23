27ff6a7e05b1fd30ab7dab9fd02fcb5fe304ff8f
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 13:04:55 +0330

    add class entity to sessionFactory

--- src/main/java/app/cfg/SessionFactoryInstance.java
+++ src/main/java/app/cfg/SessionFactoryInstance.java
@@ -1,5 +1,8 @@
 package app.cfg;
 
+import app.entity.Admin;
+import app.entity.Student;
+import app.entity.Teacher;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -9,6 +12,9 @@ public class SessionFactoryInstance {
     static {
 sessionFactory = new Configuration()
         .configure()
+        .addAnnotatedClass(Admin.class)
+        .addAnnotatedClass(Student.class)
+        .addAnnotatedClass(Teacher.class)
         .buildSessionFactory();
 
     }
