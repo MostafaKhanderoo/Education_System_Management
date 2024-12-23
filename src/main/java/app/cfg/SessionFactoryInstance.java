5f2878c74c6d5f0bf563ee190c8cf279dad82f9d
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 12:51:34 +0330

    add class to SessionFactoryInstance

--- src/main/java/app/cfg/SessionFactoryInstance.java
+++ src/main/java/app/cfg/SessionFactoryInstance.java
@@ -1,4 +1,15 @@
 package app.cfg;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class SessionFactoryInstance {
+    public static SessionFactory sessionFactory;
+
+    static {
+sessionFactory = new Configuration()
+        .configure()
+        .buildSessionFactory();
+
+    }
 }
