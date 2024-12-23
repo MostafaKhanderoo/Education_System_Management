69775909e9b2fad013430e92d5625ab0a5e45458
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 16:57:48 +0330

    hibernate for add data to database

--- src/main/java/app/repository/impl/AdminRepositoryImpl.java
+++ src/main/java/app/repository/impl/AdminRepositoryImpl.java
@@ -1,4 +1,27 @@
 package app.repository.impl;
 
-public class AdminRepositoryImpl {
+import app.entity.Admin;
+import app.repository.AdminRepository;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class AdminRepositoryImpl implements AdminRepository {
+    @Override
+    public Admin save(Session session, Admin admin) {
+        System.out.println("is okkk");
+        session.persist(admin);
+        return admin;
+
+    }
+
+    @Override
+    public void delete(Session session, Long id) {
+       session.remove(id);
+    }
+
+    @Override
+    public List<Admin> findAdmin(Session session) {
+        return null;
+    }
 }
