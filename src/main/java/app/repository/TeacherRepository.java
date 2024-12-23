b0c03c5c95ac0e4295b3202c5d4201c9481079f4
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 16:57:10 +0330

    add CRUD for Teacher

--- src/main/java/app/repository/TeacherRepository.java
+++ src/main/java/app/repository/TeacherRepository.java
@@ -1,4 +1,15 @@
 package app.repository;
 
+import app.entity.Student;
+import app.entity.Teacher;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public interface TeacherRepository {
+    Teacher save(Session session, Teacher teacher);
+    Teacher findById(Session session,Long id);
+    void deleteById(Session session,Long id);
+    List<Teacher> findAll(Session session);
+    Teacher update(Session session,Teacher teacher);
 }
