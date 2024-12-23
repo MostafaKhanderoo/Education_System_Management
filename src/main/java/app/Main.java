9b2adb627861cc441ba6a54e7b2ab4c9748e3a6e
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 17:02:19 +0330

    refactor class field and add Annotation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,32 @@
 package app;
 
+import app.entity.Admin;
+import app.entity.Student;
+import app.repository.impl.AdminRepositoryImpl;
+import app.repository.impl.StudentRepositoryImpl;
+import app.service.AdminService;
+import app.service.impl.AdminServiceImpl;
+import app.service.impl.StudentServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("test main");
+        AdminRepositoryImpl adminRepository = new AdminRepositoryImpl();
+        AdminServiceImpl adminService = new AdminServiceImpl(adminRepository);
 
+        Admin admin = new Admin("mostafa", "1234");
+
+        adminService.save(admin);
+        StudentRepositoryImpl studentRepository = new StudentRepositoryImpl();
+        StudentServiceImpl studentService = new StudentServiceImpl(studentRepository);
+
+        Student student = new Student("ali","ahmadi","ali","123456","093000","ali@gmail.com","123456",1L);
+        studentService.save(student);
+///**
+// * @param sseeso
+// *
+// * @return todo test code
+// *
+// */
     }
 }
