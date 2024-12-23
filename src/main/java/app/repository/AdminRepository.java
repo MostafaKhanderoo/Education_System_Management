84db06454698591c7cbfe4ba02e88d9426f454e5
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 16:56:33 +0330

    add CRUD for Admin

--- src/main/java/app/repository/AdminRepository.java
+++ src/main/java/app/repository/AdminRepository.java
@@ -1,4 +1,13 @@
 package app.repository;
 
+import app.entity.Admin;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public interface AdminRepository {
+    Admin save(Session session, Admin admin);
+    void delete(Session session ,Long id);
+    List<Admin> findAdmin(Session session);
 }
