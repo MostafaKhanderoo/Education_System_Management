2c7c1c48afe721c8a85c01b9c93a7a14544f17b0
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 16:56:57 +0330

    add CRUD for Student

--- src/main/java/app/repository/StudentRepository.java
+++ src/main/java/app/repository/StudentRepository.java
@@ -1,4 +1,18 @@
 package app.repository;
 
+import app.entity.Student;
+import org.hibernate.Session;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentRepository {
+    Student save(Session session,Student student);
+    Optional<Student> findById(Session session, Long id);
+    void deleteById(Session session,Long id);
+    List<Student>findAll(Session session);
+    Student update(Session session,Student student);
+
+
+
 }
