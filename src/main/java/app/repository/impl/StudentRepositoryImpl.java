72281a5d8b31eb9ab87fa8ab2dd29736c3272c26
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 17:00:51 +0330

    method CRUD for Teacher

--- src/main/java/app/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/app/repository/impl/StudentRepositoryImpl.java
@@ -1,4 +1,39 @@
 package app.repository.impl;
 
-public class StudentRepositoryImpl {
+import app.entity.Student;
+import app.repository.StudentRepository;
+import org.hibernate.Session;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StudentRepositoryImpl implements StudentRepository {
+    @Override
+    public Student save(Session session, Student student) {
+       session.persist(student);
+       return student;
+
+    }
+
+    @Override
+    public Optional<Student> findById(Session session, Long id) {
+        session.byId(Student.class).loadOptional(id);
+
+        return null;
+    }
+
+    @Override
+    public void deleteById(Session session, Long id) {
+            session.detach(id);
+    }
+
+    @Override
+    public List<Student> findAll(Session session) {
+        return null;
+    }
+
+    @Override
+    public Student update(Session session, Student student) {
+        return null;
+    }
 }
--- src/main/java/app/repository/impl/TeacherRepositoryImpl.java
+++ src/main/java/app/repository/impl/TeacherRepositoryImpl.java
@@ -1,4 +1,35 @@
 package app.repository.impl;
 
-public class TeacherRepositoryImpl {
+import app.entity.Teacher;
+import app.repository.TeacherRepository;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class TeacherRepositoryImpl implements TeacherRepository {
+    @Override
+    public Teacher save(Session session, Teacher teacher) {
+        session.persist(teacher);
+        return teacher;
+    }
+
+    @Override
+    public Teacher findById(Session session, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Session session, Long id) {
+
+    }
+
+    @Override
+    public List<Teacher> findAll(Session session) {
+        return null;
+    }
+
+    @Override
+    public Teacher update(Session session, Teacher teacher) {
+        return null;
+    }
 }
