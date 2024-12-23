5d380b51dcd1dfb11535cb33e9d685f869752518
mostafa <mostafakhanderoo7@gmail.com>
2024-12-23 12:08:27 +0330

    add all fields for Student

--- src/main/java/app/entity/Student.java
+++ src/main/java/app/entity/Student.java
@@ -1,4 +1,27 @@
 package app.entity;
 
-public class Student {
+import app.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student extends BaseEntity<Long> {
+
+    private  String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String email;
+    private String nationalCode;
+    private String studentNumber;
 }
