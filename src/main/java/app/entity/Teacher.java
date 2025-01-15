5a7f906bd83dc666bf5c531443a41774eb613eb9
mostafa <mostafakhanderoo7@gmail.com>
2025-01-15 13:10:50 +0330

    add validation size for column

--- src/main/java/app/entity/Teacher.java
+++ src/main/java/app/entity/Teacher.java
@@ -11,16 +11,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "teachers")
+
 public class Teacher extends BaseEntity<Long> {
 private String firstname;
 private String lastname;
 private  String username;
+@Size(min = 8, message = "password can,t  less than 8 word")
 private String password;
 private String phoneNumber;
 private String email;
