4cc5ec37cb520c640d0ec105147935351159fec3
mostafa <mostafakhanderoo7@gmail.com>
2025-01-15 13:31:11 +0330

    refactor methods and debug

--- src/main/java/app/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/app/repository/impl/StudentRepositoryImpl.java
@@ -2,13 +2,21 @@ package app.repository.impl;
 
 import app.entity.Student;
 import app.repository.StudentRepository;
+import app.service.impl.StudentServiceImpl;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import lombok.ToString;
 import org.hibernate.Session;
 import org.postgresql.core.Query;
 
 import java.util.List;
 import java.util.Optional;
-
+@ToString
 public class StudentRepositoryImpl implements StudentRepository {
+    StudentRepositoryImpl studentRepository;
+    StudentServiceImpl studentService;
     @Override
     public Student save(Session session, Student student) {
        session.persist(student);
@@ -18,26 +26,35 @@ public class StudentRepositoryImpl implements StudentRepository {
 
     @Override
     public Optional<Student> findById(Session session, Long id) {
-        session.byId(Student.class).loadOptional(id);
+       //session.get(Student.class,id);
+      return Optional.of(session.get(Student.class,id));
 
-        return Optional.empty();
+
     }
 
     @Override
     public void deleteById(Session session, Long id) {
-            session.remove(id);
+            session.createMutationQuery("delete from Student a where a.id = :id").setParameter("id",id).executeUpdate();
     }
 
     @Override
     public List<Student> findAll(Session session) {
-var allStudent = session.get(Student.class,findById(session,null));
-        System.out.println(allStudent);
-        return null;
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Student> cq = cb.createQuery(Student.class);
+        Root<Student> rootEntry = cq.from(Student.class);
+        CriteriaQuery<Student> all = cq.select(rootEntry);
+
+        TypedQuery<Student> allQuery = session.createQuery(all);
+        return allQuery.getResultList();
+
     }
 
     @Override
-    public Student update(Session session, Student student) {
+    public Student update(Session session,Long id, Student student) {
+
+        student.setId(id);
         session.merge(student);
-        return null;
+
+        return student;
     }
 }
