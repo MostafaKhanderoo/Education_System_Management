3ac9a5734ff3f62e9169792bc7504e5c9f2d4d35
mostafa <mostafakhanderoo7@gmail.com>
2025-01-15 13:31:42 +0330

    debug save and delete methode

--- src/main/java/app/repository/impl/TeacherRepositoryImpl.java
+++ src/main/java/app/repository/impl/TeacherRepositoryImpl.java
@@ -1,10 +1,12 @@
 package app.repository.impl;
 
+import app.entity.Student;
 import app.entity.Teacher;
 import app.repository.TeacherRepository;
 import org.hibernate.Session;
 
 import java.util.List;
+import java.util.Optional;
 
 public class TeacherRepositoryImpl implements TeacherRepository {
     @Override
@@ -14,22 +16,25 @@ public class TeacherRepositoryImpl implements TeacherRepository {
     }
 
     @Override
-    public Teacher findById(Session session, Long id) {
-        return null;
+    public Optional<Teacher> findById(Session session, Long id) {
+        return Optional.of(session.get(Teacher.class,id));
     }
 
     @Override
     public void deleteById(Session session, Long id) {
-
+            session.createMutationQuery("delete from Teacher a where a.id = :id").setParameter("id",id).executeUpdate();
     }
 
     @Override
     public List<Teacher> findAll(Session session) {
-        return null;
+        var studentList = session.createMutationQuery("from Student");
+        return (List<Teacher>) studentList;
     }
 
     @Override
-    public Teacher update(Session session, Teacher teacher) {
+    public Teacher update(Session session,Long id, Teacher teacher) {
+        teacher.setId(id);
+        session.merge(teacher);
         return null;
     }
 }
