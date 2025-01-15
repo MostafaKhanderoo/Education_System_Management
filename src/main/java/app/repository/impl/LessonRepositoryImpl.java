49a6d9575d812b5142bfddd95161eec5268ec7bf
mostafa <mostafakhanderoo7@gmail.com>
2025-01-15 13:22:32 +0330

    refactor And add CRUD methods for lesson

--- src/main/java/app/repository/impl/LessonRepositoryImpl.java
+++ src/main/java/app/repository/impl/LessonRepositoryImpl.java
@@ -4,10 +4,44 @@ import app.entity.Lesson;
 import app.repository.LessonRepository;
 import org.hibernate.Session;
 
+import java.util.List;
+import java.util.Optional;
+
 public class LessonRepositoryImpl implements LessonRepository {
     @Override
-    public Lesson save(Session session, Lesson lesson) {
+    public Lesson addNewLesson(Session session, Lesson lesson) {
         session.persist(lesson);
         return lesson;
     }
+
+    @Override
+    public void deleteLessen(Session session, Long id) {
+        session.createMutationQuery("delete from Lesson a where a.id = :id ").setParameter("id",id).executeUpdate();
+    }
+
+    @Override
+    public Lesson update(Session session, Long id, Lesson lesson) {
+        lesson.setId(id);
+        session.merge(lesson);
+        return lesson;
+    }
+
+    @Override
+    public List<Lesson> ShowAllLessonAvailable(Session session) {
+         List<Lesson> lessons = session.createQuery("from Lesson ",Lesson.class).list();
+         return lessons;
+    }
+
+    @Override
+    public Optional<Lesson> showLessonById(Session session, Long id) {
+        //var lessons= session.createMutationQuery(" from Lesson a where a.id= :id").setParameter("id",id);
+        return Optional.of(session.get(Lesson.class,id));
+    }
+
+    @Override
+    public List<Lesson> showLessonByName(Session session, String name) {
+
+        return null;
+    }
+
 }
