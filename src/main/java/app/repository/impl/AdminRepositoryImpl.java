9a026a98fd824c2551189adcc695ce5d01fe4144
mostafa <mostafakhanderoo7@gmail.com>
2025-01-19 14:16:26 +0330

    debug and add methode find by Username and login

--- src/main/java/app/repository/impl/AdminRepositoryImpl.java
+++ src/main/java/app/repository/impl/AdminRepositoryImpl.java
@@ -1,10 +1,13 @@
 package app.repository.impl;
 
+import app.cfg.SessionFactoryInstance;
 import app.entity.Admin;
 import app.repository.AdminRepository;
+import app.service.Authentication.AuthenticationAdmin;
 import org.hibernate.Session;
 
 import java.util.List;
+import java.util.Locale;
 
 public class AdminRepositoryImpl implements AdminRepository {
     @Override
@@ -16,11 +19,37 @@ public class AdminRepositoryImpl implements AdminRepository {
 
     @Override
     public void delete(Session session, Long id) {
-       session.createMutationQuery("delete from Admin a where a.id =:id").setParameter("id",id).executeUpdate();
+        session.createMutationQuery("delete from Admin a where a.id =:id").setParameter("id", id).executeUpdate();
     }
 
     @Override
     public List<Admin> selectAllAdmin(Session session) {
-        return session.createQuery("from Admin",Admin.class).list();
+        List<Admin> allAdmin= session.createQuery("from Admin", Admin.class).list();
+        return allAdmin;
+    }
+
+    @Override
+    public Admin findByUsername(Session session, String username) {
+        Admin admin = session.createQuery(" from Admin where username = :username", Admin.class).setParameter("username", username).getSingleResult();
+        return admin;
+    }
+
+    @Override
+    public Admin login(Session session, String username, String password) {
+        var admin= findByUsername(session,username);
+      try  {
+          if (admin.getUsername().equals(username))
+              if (admin.getPassword().equals(password)) {
+                  AuthenticationAdmin.setLoggedAdmin(admin);
+                  return admin;
+              } else {
+                  System.out.println("error");
+
+              }
+      }catch (Exception e){
+          throw new RuntimeException(e.getMessage());
+
+      }
+        return null;
     }
 }
