e01e2eeac76dbb3cebe05c5773b02d577defb9b2
mostafa <mostafakhanderoo7@gmail.com>
2025-01-19 14:13:57 +0330

    add new Annotation lombok and many to many with teacher

--- src/main/java/app/entity/Lesson.java
+++ src/main/java/app/entity/Lesson.java
@@ -1,14 +1,13 @@
 package app.entity;
 
 import app.base.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -21,4 +20,6 @@ public class Lesson extends BaseEntity<Long> {
 private LocalDateTime startLessonTime;
 @ManyToOne
 private Teacher teacher;
+@ManyToMany
+    List<Student> students;
 }
