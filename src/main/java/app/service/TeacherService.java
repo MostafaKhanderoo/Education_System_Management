b4755e996f50b1819ed4ed852147645d1f9bb1d8
mostafa <mostafakhanderoo7@gmail.com>
2025-01-22 20:19:51 +0330

    change id to studentNumber for update student

--- src/main/java/app/service/TeacherService.java
+++ src/main/java/app/service/TeacherService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface TeacherService {
     Teacher save (Teacher teacher);
-    Teacher update(Long id ,Teacher teacher);
+    Teacher update(Long personnelCode ,Teacher teacher);
     void deleteTeacher(Long id);
     Teacher findTeacherById(Long id );
     List<Teacher> showAllTeacher();
