767c7071a3cbba9e6f8e709f7541919332b0584f
mostafa <mostafakhanderoo7@gmail.com>
2025-01-22 20:19:25 +0330

    change id to studentNumber for delete

--- src/main/java/app/service/StudentService.java
+++ src/main/java/app/service/StudentService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface StudentService {
     Student save(Student student);
     Student findById(Long id);
-    void deleteById(Long id);
+    void deleteByStudentNumber(Long studentNumber);
     List<Student>findAll();
     Student update(Long id,Student student);
 }
