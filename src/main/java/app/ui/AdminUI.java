99f2a59477adb91866908a2adbdf13b64859c4b0
mostafa <mostafakhanderoo7@gmail.com>
2025-01-22 20:51:22 +0330

    Complete ui admin for log admin in software

--- src/main/java/app/ui/AdminUI.java
+++ src/main/java/app/ui/AdminUI.java
@@ -1,6 +1,6 @@
 package app.ui;
 
-import app.Main;
+import app.entity.Lesson;
 import app.entity.Student;
 import app.entity.Teacher;
 import app.enumeration.Degree;
@@ -15,6 +15,7 @@ import app.service.impl.LessonServiceImpl;
 import app.service.impl.StudentServiceImpl;
 import app.service.impl.TeacherServiceImpl;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class AdminUI {
@@ -46,13 +47,13 @@ public class AdminUI {
             onlineAdmin = false;
         }
         boolean onlineAdmin1 = true;
-        System.out.println("1.Teachers Management Menu");
-        System.out.println("2.Students Management Menu");
-        System.out.println("3.Lessons Management Menu");
-        System.out.println("4.Logout");
-        System.out.print("choose: ");
-        while (onlineAdmin1) {
 
+        while (onlineAdmin1) {
+            System.out.println("1.Teachers Management Menu");
+            System.out.println("2.Students Management Menu");
+            System.out.println("3.Lessons Management Menu");
+            System.out.println("4.Logout");
+            System.out.print("choose: ");
             int input = scanner.nextInt();
             switch (input) {
                 case 1:
@@ -82,22 +83,22 @@ public class AdminUI {
     private static void addNewTeacher() {
         Teacher teacher = new Teacher();
         System.out.print("firstname: ");
-        String firstname = scanner.nextLine();
+        String firstname = scanner.next();
         System.out.print("lastname: ");
-        String lastname = scanner.nextLine();
+        String lastname = scanner.next();
         System.out.print("username: ");
-        String username = scanner.nextLine();
+        String username = scanner.next();
         System.out.print("password: ");
-        String password = scanner.nextLine();
+        String password = scanner.next();
         System.out.print("phoneNumber: ");
-        String phoneNumber = scanner.nextLine();
+        String phoneNumber = scanner.next();
         System.out.print("email: ");
-        String email = scanner.nextLine();
+        String email = scanner.next();
         System.out.print("nationalCode: ");
-        String nationalCode = scanner.nextLine();
+        String nationalCode = scanner.next();
         Expertise expertise;
         Degree degree;
-
+        System.out.print("personnelCode: ");
         Long personnelCode = scanner.nextLong();
         teacher.setFirstname(firstname);
         teacher.setLastname(lastname);
@@ -114,11 +115,55 @@ public class AdminUI {
         teacherService.save(teacher);
     }
 
+    static void removeTeacher() {
+        System.out.println(teacherService.showAllTeacher());
+        System.out.println("choose one teacher by personnelCode");
+        Long personnelCode = scanner.nextLong();
+        teacherService.deleteTeacher(personnelCode);
+    }
+
+    private static void updateTeacher() {
+        System.out.println(teacherService.showAllTeacher());
+        System.out.println();
+        System.out.print("enter personnelCode teacher to update");
+        Long personnelCode = scanner.nextLong();
+        Teacher teacher = new Teacher();
+        System.out.print("firstname: ");
+        String firstname = scanner.next();
+        System.out.print("lastname: ");
+        String lastname = scanner.next();
+        System.out.print("username: ");
+        String username = scanner.next();
+        System.out.print("password: ");
+        String password = scanner.next();
+        System.out.print("phoneNumber: ");
+        String phoneNumber = scanner.next();
+        System.out.print("email: ");
+        String email = scanner.next();
+        System.out.print("nationalCode: ");
+        String nationalCode = scanner.next();
+        Expertise expertise;
+        Degree degree;
+        teacher.setFirstname(firstname);
+        teacher.setLastname(lastname);
+        teacher.setUsername(username);
+        teacher.setPassword(password);
+        teacher.setPhoneNumber(phoneNumber);
+        teacher.setEmail(email);
+        teacher.setNationalCode(nationalCode);
+        System.out.print("Expertise: ");
+        teacher.setExpertise(Expertise.ENGINEERING);
+        System.out.print("Degree: ");
+        teacher.setDegree(Degree.TEST1);
+        teacherService.update(personnelCode, teacher);
+    }
+
     private static void teacherManagementMenu() {
         System.out.println("1.See All Teacher");
         System.out.println("2.Add New Teacher");
         System.out.println("3.Delete Teacher");
-        System.out.println("4.Update Teacher Profile");
+        System.out.println("4.add lesson for Teacher");
+        System.out.println("5.Update Teacher Profile");
         System.out.print("choose: ");
         int input = scanner.nextInt();
         Boolean onlineInTeacherMenu = true;
@@ -131,13 +176,37 @@ public class AdminUI {
                 case 2:
                     addNewTeacher();
                     onlineInTeacherMenu = false;
-
-
+                    break;
+                case 3:
+                    removeTeacher();
+                    onlineInTeacherMenu = false;
+                    break;
+                case 4:
+                    setLessonForTeacher();
+                    onlineInTeacherMenu = false;
+                    break;
+                case 5:
+                    updateTeacher();
+                    onlineInTeacherMenu = false;
                     break;
                 default:
                     System.out.println("invalid number");
             }
         }
+    }
+
+    static void setLessonForTeacher() {
+        System.out.println(lessonService.AllLesson());
+        System.out.println();
+        System.out.print("choose one lesson by id: ");
+        Long lessonId = scanner.nextLong();
+        System.out.println(teacherService.showAllTeacher());
+        System.out.println();
+        System.out.print("choose one teacher by id: ");
+        Long teacherId = scanner.nextLong();
+        lessonService.setTeacherForLesson(lessonId, teacherId);
+        System.out.println("added");
+
     }
 
     //static  void addNewStudent(){
@@ -151,15 +220,101 @@ public class AdminUI {
         System.out.println();
         System.out.println();
         System.out.print("enter student id: ");
-        Long id = scanner.nextLong();
-        studentService.deleteById(id);
+        Long studentNumber = scanner.nextLong();
+        studentService.deleteByStudentNumber(studentNumber);
     }
+
+    static void seeLessonsOfStudent() {
+        System.out.println(studentService.findAll());
+        System.out.println();
+        System.out.print("enter student studentNumber: ");
+        Long studentNumber = scanner.nextLong();
+        System.out.println("lessons of student");
+        System.out.println(studentService.seeLessonByStudentNumber(studentNumber));
+    }
+
+    static void seeStudentsOfLessonNumber() {
+        System.out.println(lessonService.AllLesson());
+        System.out.println();
+        System.out.print("enter lessonNumber");
+        Long lessonNumber = scanner.nextLong();
+        System.out.println(studentService.seeStudentByLessonNumber(lessonNumber));
+    }
+
+    static void addNewStudent() {
+        System.out.println("create new student profile");
+        Student student = new Student();
+        System.out.print("firstName: ");
+        String firstName = scanner.next();
+        System.out.print("lastName: ");
+        String lastName = scanner.next();
+        System.out.print("username: ");
+        String username = scanner.next();
+        System.out.print("password: ");
+        String password = scanner.next();
+        System.out.print("phoneNumber: ");
+        String phoneNumber = scanner.next();
+        System.out.print("email: ");
+        String email = scanner.next();
+        System.out.print("nationalCode: ");
+        String nationalCode = scanner.next();
+        System.out.print("studentNumber: ");
+        Long studentNumber = scanner.nextLong();
+        student.setStudentNumber(studentNumber);
+        student.setUsername(username);
+        student.setNationalCode(nationalCode);
+        student.setEmail(email);
+        student.setFirstname(firstName);
+        student.setPhoneNumber(phoneNumber);
+        student.setPassword(password);
+        student.setLastname(lastName);
+        studentService.save(student);
+        System.out.println("student saved");
+
+
+    }
+
+    static void updateStudent() {
+        System.out.println("update student profile");
+        Student student = new Student();
+        System.out.println(studentService.findAll());
+        System.out.print("enter studentNumber to update");
+        Long studentNumber = scanner.nextLong();
+        System.out.print("firstName: ");
+        String firstName = scanner.next();
+        System.out.print("lastName: ");
+        String lastName = scanner.next();
+        System.out.print("username: ");
+        String username = scanner.next();
+        System.out.print("password: ");
+        String password = scanner.next();
+        System.out.print("phoneNumber: ");
+        String phoneNumber = scanner.next();
+        System.out.print("email: ");
+        String email = scanner.next();
+        System.out.print("nationalCode: ");
+        String nationalCode = scanner.next();
+        student.setUsername(username);
+        student.setNationalCode(nationalCode);
+        student.setEmail(email);
+        student.setFirstname(firstName);
+        student.setPhoneNumber(phoneNumber);
+        student.setPassword(password);
+        student.setLastname(lastName);
+        studentService.update(studentNumber, student);
+        System.out.println("student updated");
+
+
+    }
+
 
     private static void studentManagementMenu() {
         System.out.println("1.See All students");
-        System.out.println("2.Add New student");
+        System.out.println("2.Add New student ");
         System.out.println("3.Delete student");
-        System.out.println("4.Update student Profile");
+        System.out.println("4.see lessons Of student");
+        System.out.println("5.see students Of lesson");
+        System.out.println("6.Update student Profile");
         System.out.println("5.log out");
         System.out.print("choose: ");
         boolean onlineInStudentMenu = true;
@@ -167,12 +322,30 @@ public class AdminUI {
         while (onlineInStudentMenu) {
             switch (input) {
                 case 1:
-                    studentService.findAll();
+                    System.out.println(studentService.findAll());
+                    onlineInStudentMenu = false;
+                    break;
                 case 2:
+                    addNewStudent();
+                    onlineInStudentMenu = false;
                     break;
 
                 case 3:
                     deleteStudent();
+                    onlineInStudentMenu = false;
+                    break;
+                case 4:
+                    seeLessonsOfStudent();
+                    onlineInStudentMenu = false;
+                    break;
+                case 5:
+                    seeStudentsOfLessonNumber();
+                    onlineInStudentMenu = false;
+                    break;
+                case 6:
+
+                    updateStudent();
+                    onlineInStudentMenu = false;
                     break;
 
                 default:
@@ -181,6 +354,57 @@ public class AdminUI {
             }
 
         }
+    }
+
+    static void createNewLesson() {
+        Lesson lesson = new Lesson();
+        System.out.print("lessonName: ");
+        String lessonName = scanner.next();
+        System.out.print("util: ");
+        int util = scanner.nextInt();
+        System.out.print("capacity: ");
+        int capacity = scanner.nextInt();
+        System.out.print("LessonNumber: ");
+        Long lessonNumber = scanner.nextLong();
+        lesson.setLessonName(lessonName);
+        lesson.setCapacity(capacity);
+        lesson.setUtil(util);
+        lesson.setLessonNumber(lessonNumber);
+        lesson.setStartLessonTime(LocalDateTime.now());
+        lessonService.save(lesson);
+        System.out.println("lesson saved");
+    }
+
+    static void removeLesson() {
+        System.out.println(lessonService.AllLesson());
+        System.out.println();
+        System.out.print("enter lessonNumber to delete: ");
+        Long lessonNumber = scanner.nextLong();
+        lessonService.deleteLesson(lessonNumber);
+
+    }
+
+    static void updateLessonProfile() {
+        Lesson lesson = new Lesson();
+        System.out.println(lessonService.AllLesson());
+
+        System.out.print("enter lesson Number to update ");
+        Long lessonNumber = scanner.nextLong();
+
+        System.out.print("lessonName: ");
+        String lessonName = scanner.next();
+        System.out.print("util: ");
+        int util = scanner.nextInt();
+        System.out.print("capacity: ");
+        int capacity = scanner.nextInt();
+
+        lesson.setLessonName(lessonName);
+        lesson.setCapacity(capacity);
+        lesson.setUtil(util);
+        lesson.setLessonNumber(lessonNumber);
+        lesson.setStartLessonTime(LocalDateTime.now());
+        lessonService.updateLesson(lessonNumber, lesson);
+        System.out.println("lesson saved");
     }
 
     private static void lessonManagementMenu() {
@@ -196,19 +420,29 @@ public class AdminUI {
         while (onlineInLessonMenu) {
             switch (input) {
                 case 1:
-                    lessonService.AllLesson();
-                    break;
-                case 2:
-                    //lessonService.save();
-                    break;
-                case 3:
+                    System.out.println(lessonService.AllLesson());
+                    System.out.println();
+                    onlineInLessonMenu = false;
                     break;
 
+                case 2:
+                    createNewLesson();
+                    onlineInLessonMenu = false;
+                    break;
+                case 3:
+                    removeLesson();
+                    onlineInLessonMenu = false;
+                    break;
+                case 4:
+                    updateLessonProfile();
+                    onlineInLessonMenu = false;
+                    break;
+                case 5:
+                    onlineInLessonMenu = false;
                 default:
                     System.out.println("invalid number");
 
-
-            }
+            }return;
         }
     }
 }
