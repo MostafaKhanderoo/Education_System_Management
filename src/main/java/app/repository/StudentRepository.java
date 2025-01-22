e15c3474b42f82b08d93084ee39d3047716f30ee
mostafa <mostafakhanderoo7@gmail.com>
2025-01-22 20:07:12 +0330

    change methode name delete by student number

--- src/main/java/app/repository/StudentRepository.java
+++ src/main/java/app/repository/StudentRepository.java
@@ -9,9 +9,10 @@ import java.util.Optional;
 public interface StudentRepository {
     Student save(Session session,Student student);
     Optional<Student> findById(Session session, Long id);
-    void deleteById(Session session,Long id);
+    void deleteByStudentNumber(Session session,Long studentNumber);
     List<Student>findAll(Session session);
     Student update(Session session,Long id,Student student);
+    boolean login(Session session,Long studentNumber,String password);
 
 
 
