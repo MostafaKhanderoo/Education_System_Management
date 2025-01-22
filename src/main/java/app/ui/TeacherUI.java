fe3d1a91fff28592c0d8d6a9e1e751860c0e4ddd
mostafa <mostafakhanderoo7@gmail.com>
2025-01-22 20:52:46 +0330

    Complete ui teacher for log teacher in software

--- src/main/java/app/ui/TeacherUI.java
+++ src/main/java/app/ui/TeacherUI.java
@@ -1,4 +1,31 @@
 package app.ui;
 
+import app.repository.impl.TeacherRepositoryImpl;
+import app.service.impl.TeacherServiceImpl;
+
+import java.util.Scanner;
+
 public class TeacherUI {
+final  private  static Scanner scanner = new Scanner(System.in);
+final private static TeacherRepositoryImpl teacherRepository = new TeacherRepositoryImpl();
+final private static TeacherServiceImpl teacherService =  new TeacherServiceImpl(teacherRepository);
+    public  static  void teacherMenu(){
+        System.out.println("teacher login");
+        System.out.print("enter your personnelCode: ");
+        Long personnelCode =scanner.nextLong();
+        System.out.println("enter your password");
+        String password =scanner.next();
+
+        var test= teacherService.login(personnelCode,password);
+
+        while (test){
+            System.out.println("1.see my lessons added");
+            System.out.println("2.set a course for students");
+            System.out.println("3.update profile");
+            System.out.println("4.logout");
+
+        }
+
+
+    }
 }
