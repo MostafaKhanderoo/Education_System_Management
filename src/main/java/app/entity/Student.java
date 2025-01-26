ad1d84c3da0f920e926891ec3a2aaeb6096d44e3
mostafa <mostafakhanderoo7@gmail.com>
2025-01-27 00:55:36 +0330

    add annotation to field for handel in Db

--- src/main/java/app/entity/Student.java
+++ src/main/java/app/entity/Student.java
@@ -3,7 +3,11 @@ package app.entity;
 import app.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UniqueElements;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
@@ -17,20 +21,32 @@ import java.util.List;
 public class Student extends BaseEntity<Long> {
 
     @Column(nullable = false)
+    @Size(max = 30)
     private String firstname;
     @Column(nullable = false)
+    @Size(max = 30)
     private String lastname;
-    @Column(nullable = false)
+    @Column(nullable = false ,unique = true)
+@UniqueElements(message = "this username is not available")
     private String username;
+
     @Column(nullable = false)
+    @Size(min = 8)
     private String password;
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
+    @Size(min = 10,max = 11)
+
     private String phoneNumber;
-    @Column
+    @Column(unique = true)
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
     private String email;
-    @Column(nullable = false)
+
+
+    @Column(nullable = false ,length = 10,unique = true)
+    @Size(min = 10)
     private String nationalCode;
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private Long studentNumber;
 
 
