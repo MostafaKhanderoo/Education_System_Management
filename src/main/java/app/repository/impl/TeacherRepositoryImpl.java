5b73212829b78d6eb92e7586b35222382198da5f
mostafa <mostafakhanderoo7@gmail.com>
2025-01-27 01:01:33 +0330

    add new repository for teacher

--- src/main/java/app/repository/impl/TeacherRepositoryImpl.java
+++ src/main/java/app/repository/impl/TeacherRepositoryImpl.java
@@ -2,12 +2,14 @@ package app.repository.impl;
 
 import app.entity.Lesson;
 import app.entity.Student;
+import app.entity.StudentLesson;
 import app.entity.Teacher;
 import app.repository.TeacherRepository;
-import app.service.Authentication.AuthenticationStudent;
 import app.service.Authentication.AuthenticationTeacher;
+import jakarta.persistence.NoResultException;
 import org.hibernate.Session;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,43 +17,56 @@ public class TeacherRepositoryImpl implements TeacherRepository {
     @Override
     public Teacher save(Session session, Teacher teacher) {
         session.persist(teacher);
+        teacher.setUsername(teacher.getPersonnelCode().toString());
+        teacher.setPassword(teacher.getNationalCode());
         return teacher;
     }
 
     @Override
     public Optional<Teacher> findById(Session session, Long id) {
-        return Optional.of(session.get(Teacher.class,id));
+        return Optional.of(session.get(Teacher.class, id));
     }
 
     @Override
     public void deleteByPersonnelCode(Session session, Long personnelCode) {
-            session.createMutationQuery("delete from Teacher a where a.personnelCode = :personnelCode").setParameter("personnelCode",personnelCode).executeUpdate();
+        session.createMutationQuery("delete from Teacher a where a.personnelCode = :personnelCode").setParameter("personnelCode", personnelCode).executeUpdate();
     }
 
     @Override
     public List<Teacher> findAll(Session session) {
-        List<Teacher> studentList = session.createQuery("from Teacher",Teacher.class).list();
+        List<Teacher> studentList = session.createQuery("from Teacher", Teacher.class).list();
         return studentList;
     }
 
     @Override
-    public Teacher update(Session session,Long personnelCode, Teacher teacher) {
+    public Teacher update(Session session, Long personnelCode, Teacher teacher) {
         teacher.setPersonnelCode(personnelCode);
         session.merge(teacher);
         return teacher;
     }
+
     public boolean login(Session session, Long personnelCode, String password) {
-        var teacher = session.createQuery("from Teacher a where a.personnelCode =:personnelCode",Teacher.class).setParameter("personnelCode",personnelCode ).getSingleResult();
-        if (teacher.getPersonnelCode().equals(personnelCode) && (teacher.getPassword().equals(password))){
+        var teacher = session.createQuery("from Teacher a where a.personnelCode =:personnelCode", Teacher.class).setParameter("personnelCode", personnelCode).getSingleResult();
+        if (teacher.getPersonnelCode().equals(personnelCode) && (teacher.getPassword().equals(password))) {
             AuthenticationTeacher.setLoggedTeacher(teacher);
             return true;
-        }
-        else
+        } else
             System.out.println("personnelCode or password is wrong!");
         return false;
     }
-    public List<Lesson>  lessonsForTeacher(Session session,Long personnelCode){
-     List<Lesson>lessonsOfTeacher =  session.createQuery("from  Lesson a where a.teacher.personnelCode = :personnelCode",Lesson.class).setParameter("personnelCode",personnelCode).list();
-     return lessonsOfTeacher;
+
+    public List<Lesson> lessonsForTeacher(Session session, Long personnelCode) {
+        List<Lesson> lessonsOfTeacher = session.createQuery("from  Lesson a where a.teacher.personnelCode = :personnelCode", Lesson.class).setParameter("personnelCode", personnelCode).list();
+        return lessonsOfTeacher;
     }
+
+
+    public void changeUsernameAmdPassword(Session session,Long personnelCode, String username,String password){
+        var student= session.createQuery("from Teacher a where a.personnelCode = :personnelCode",Teacher.class).setParameter("personnelCode",personnelCode).getSingleResult();
+        student.setUsername(username);
+        student.setPassword(password);
+    }
+
+
+
 }
