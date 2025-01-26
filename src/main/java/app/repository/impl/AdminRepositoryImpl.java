f267575c7a69a1e68f71e46bde18316beb28478c
mostafa <mostafakhanderoo7@gmail.com>
2025-01-27 00:57:22 +0330

    debug and refactor

--- src/main/java/app/repository/impl/AdminRepositoryImpl.java
+++ src/main/java/app/repository/impl/AdminRepositoryImpl.java
@@ -1,13 +1,12 @@
 package app.repository.impl;
 
-import app.cfg.SessionFactoryInstance;
 import app.entity.Admin;
 import app.repository.AdminRepository;
 import app.service.Authentication.AuthenticationAdmin;
+import jakarta.persistence.NoResultException;
 import org.hibernate.Session;
 
 import java.util.List;
-import java.util.Locale;
 
 public class AdminRepositoryImpl implements AdminRepository {
     @Override
@@ -17,6 +16,7 @@ public class AdminRepositoryImpl implements AdminRepository {
 
     }
 
+
     @Override
     public void delete(Session session, Long id) {
         session.createMutationQuery("delete from Admin a where a.id =:id").setParameter("id", id).executeUpdate();
@@ -24,32 +24,39 @@ public class AdminRepositoryImpl implements AdminRepository {
 
     @Override
     public List<Admin> selectAllAdmin(Session session) {
-        List<Admin> allAdmin= session.createQuery("from Admin", Admin.class).list();
+        List<Admin> allAdmin = session.createQuery("from Admin", Admin.class).list();
         return allAdmin;
     }
 
     @Override
     public Admin findByUsername(Session session, String username) {
-        Admin admin = session.createQuery(" from Admin where username = :username", Admin.class).setParameter("username", username).getSingleResult();
-        return admin;
+        try {
+            Admin admin = session.createQuery(" from Admin where username = :username", Admin.class).setParameter("username", username).getSingleResult();
+            return admin;
+        } catch (NoResultException e) {
+            System.out.println("no result found for " + username);
+            e.printStackTrace();
+            throw e;
+        }
+
     }
 
     @Override
     public Admin login(Session session, String username, String password) {
-        var admin= findByUsername(session,username);
-      try  {
-          if (admin.getUsername().equals(username))
-              if (admin.getPassword().equals(password)) {
-                  AuthenticationAdmin.setLoggedAdmin(admin);
-                  return admin;
-              } else {
-                  System.out.println("error");
+        var admin = findByUsername(session, username);
+        try {
+            if (admin.getUsername().equals(username))
+                if (admin.getPassword().equals(password)) {
+                    AuthenticationAdmin.setLoggedAdmin(admin);
+                    return admin;
+                } else {
+                    System.out.println("error");
 
-              }
-      }catch (Exception e){
-          throw new RuntimeException(e.getMessage());
+                }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
 
-      }
+        }
         return null;
     }
 }
