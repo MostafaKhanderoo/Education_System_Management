339eda5e6519bfd0146cca99d9e662370bee107e
mostafa <mostafakhanderoo7@gmail.com>
2025-01-27 17:48:33 +0330

    add annotation Size

--- src/main/java/app/entity/Lesson.java
+++ src/main/java/app/entity/Lesson.java
@@ -4,6 +4,7 @@ import app.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 @AllArgsConstructor
@@ -17,9 +18,13 @@ import java.util.List;
 public class Lesson extends BaseEntity<Long> {
 
     private String lessonName;
+    @Size(min = 1,max = 3,message = "util lesson must be have 1 to 3 ")
     private int util;
+    @Size(min = 10,max = 40)
     private int capacity;
+    @Temporal(TemporalType.DATE)
 private LocalDateTime startLessonTime;
+
 private Long lessonNumber;
 @ManyToOne
 private Teacher teacher;
